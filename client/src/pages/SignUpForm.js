eeb70cf2f1c06d6bd0cc2c44737cb296384c5e7e
Anne McCorkle <annefmccorkle@gmail.com>
2022-10-03 10:31:49 -0600

    adding page routes to App.js

--- client/src/pages/SignUpForm.js
+++ client/src/pages/SignUpForm.js
@@ -1,1 +1,8 @@
 //component to render signup dialog
+import React from 'react'
+
+export default function SignUpForm() {
+  return (
+    <div>SignUpForm</div>
+  )
+}
--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -1,1 +1,11 @@
 // display feed of bets from friend network
+import React from 'react';
+
+
+export default function Feed() {
+  return (
+    <div className="App">
+      <h1 className="title">YouBetcha!</h1>
+    </div> 
+  )
+}
