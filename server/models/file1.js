0e8bef32ee5547d6374ccec9ca117607cfc68a6e
Brisco Arechederra <brisco.arch@gmail.com>
2022-10-05 21:22:42 -0600

    cleaned server and root directories

---
+++ server/models/file1.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const file1Schema = new Schema({
+  attribute1: {
+    type: String,
+    required: true,
+  },
+  attribute2: {
+    type: String,
+    required: true,
+  },
+  attribute3: {
+    type: Number,
+  },
+  attribute4: {
+    type: Number,
+  },
+});
+
+const file1 = model('file1', file1Schema);
+
+module.exports = file1;
--- server/server.js
+++ server/server.js
@@ -39,4 +39,3 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
 // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
- 
