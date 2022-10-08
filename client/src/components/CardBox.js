ddeee411a0343b65a2ac929137799edb0821977c
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-10-08 13:53:37 -0600

    styling the cards

--- client/src/components/CardBox.js
+++ client/src/components/CardBox.js
@@ -1,18 +1,43 @@
-import * as React from 'react';
- 
-import { Card, CardHeader, CardActions, IconButton, TextareaAutosize, Button, Container, Typography, Box, Paper, CardContent, CardMedia } from '@mui/material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import HeartBrokenIcon from '@mui/icons-material/HeartBroken';
+import * as React from "react";
+
+import {
+  Card,
+  CardHeader,
+  CardActions,
+  IconButton,
+  TextareaAutosize,
+  Button,
+  Avatar,
+  Typography,
+  Box,
+  Paper,
+  CardContent,
+  CardMedia,
+} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import HeartBrokenIcon from "@mui/icons-material/HeartBroken";
 
 export default function CardBox() {
   return (
-    <Card raised variant="outlined" sx={{ minWidth: 275, margin: 5, border: 2}}>
-        <CardHeader
-          title="Brisco bet Anne"
-          subheader="createdAt"
-          sx={{border: 1}}
-        > {/* {betTitle} */}</CardHeader>
-        <CardMedia 
+    <Card
+      display="flex"
+      raised
+      variant="outlined"
+      sx={{ margin: 5, border: 2 }}
+    >
+      <CardHeader
+        avatar={
+          <Avatar>
+            B
+          </Avatar>
+        }
+        title="Brisco bet Anne"
+        subheader="createdAt"
+      />
+        {/* {" "} */}
+        {/* {betTitle} */}
+      
+      {/* <CardMedia 
           component="img"
           height="194"
           image="http://via.placeholder.com/75"
@@ -20,33 +45,28 @@ export default function CardBox() {
           variant="outlined"
           sx={{ border: 1 }}
           >
-        </CardMedia>
-        <CardContent>
-          <Typography sx={{ fontSize: 14}} gutterBottom>
-          the bet description: race mountain bike and then chug coke starlight  
+        </CardMedia> */}
+      <CardContent>
+        <Typography sx={{ fontSize: 12 }} gutterBottom>
+          the bet description: race mountain bike and then chug coke starlight
           {/* {betDescription} */}
-          </Typography>
-
-        </CardContent>
-        <CardActions disableSpacing>
-        <IconButton
-          aria-label="heart reaction">
-            <FavoriteIcon />
-          </IconButton>   
-        <IconButton
-          aria-label="heart breaking">
-            <HeartBrokenIcon />
-          </IconButton>
-        <TextareaAutosize 
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="heart reaction">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="heart breaking">
+          <HeartBrokenIcon />
+        </IconButton>
+        <TextareaAutosize
           aria-label="comment"
           value="comment"
           name="comment"
           placeholder="comment"
           style={{ width: 200 }}
-          
-        >
-          </TextareaAutosize>
-        </CardActions> 
-    </Card> 
-  )
+        ></TextareaAutosize>
+      </CardActions>
+    </Card>
+  );
 }
